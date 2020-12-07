c9eff80be1d0d6601aa4092f34c1c2b125399ece
quchangz <70827473+quchangz@users.noreply.github.com>
2020-12-06 18:13:52 -0600

    Update CreateUser.php

--- Lab10/CreateUser.php
+++ Lab10/CreateUser.php
@@ -1,11 +1,11 @@
 <?php
 $mysqli = new mysqli("mysql.eecs.ku.edu", "quchanzhang", "nech7eji", "quchanzhang");
 
-if ($mysqli->connect_errno) 
-{
-    printf("Connect failed: %s\n", $mysqli->connect_error);
-    exit();
-}
+    if ($mysqli->connect_errno) 
+    {
+        printf("Connect failed: %s\n", $mysqli->connect_error);
+        exit();
+    }
 
 $user = $_POST["id"];
 $sql = "INSERT INTO users (user_id) VALUES ('$user');";
