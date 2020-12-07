bc57b0a5f4606eb1d61e89a7608c1c3685f1f422
quchangz <70827473+quchangz@users.noreply.github.com>
2020-12-06 18:14:38 -0600

    Update CreateUser.php

--- Lab10/CreateUser.php
+++ Lab10/CreateUser.php
@@ -1,25 +1,25 @@
 <?php
 $mysqli = new mysqli("mysql.eecs.ku.edu", "quchanzhang", "nech7eji", "quchanzhang");
 
-    if ($mysqli->connect_errno) 
-    {
-        printf("Connect failed: %s\n", $mysqli->connect_error);
-        exit();
-    }
+if ($mysqli->connect_errno) 
+{
+    printf("Connect failed: %s\n", $mysqli->connect_error);
+    exit();
+}
 
 $user = $_POST["id"];
 $sql = "INSERT INTO users (user_id) VALUES ('$user');";
 
-    if ($mysqli->query($sql) === TRUE)
-    {
-      echo "<h4>User created successfully</h4>";
-    }
-    else
-    {
-      echo "<h4>User not created or user already exists</h4>";
-    } 
+if ($mysqli->query($sql) === TRUE)
+{
+    echo "<h4>User created successfully</h4>";
+}
+else
+{
+    echo "<h4>User not created or user already exists</h4>";
+} 
 
-    echo "<br><a href='AdminHome.html'>Back to Admin Home </a>";
+echo "<br><a href='AdminHome.html'>Back to Admin Home </a>";
   
 $mysqli->close();
 ?>
